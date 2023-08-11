67a4077822fb273925939fd303c1df5777f3787e
Brad Akemi <mirado.akemi@gmail.com>
2023-08-11 13:24:12 +0300

    Base de triage sur la page Market

--- Components/Market/ProduitsTrie.tsx
+++ Components/Market/ProduitsTrie.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { FC } from "react";
-import { fetchProduct } from '../API/API'
+import { fetchProductCat } from '../API/API'
 import Link from "next/link";
+import axios from 'axios';
 
 interface PoduitsTrieProps {
 
@@ -18,61 +19,58 @@ interface Product {
     category: number;
 }
 
-const PoduitsTrie: FC<PoduitsTrieProps> = ({ }) => {
 
-    const [products, setProducts] = useState<Product[]>([]);
+
+const PoduitsTrie: FC<PoduitsTrieProps> = ({ }) => {
+    const [products, setProduct] = useState<Product[] | null>(null);
     const urlParams = new URLSearchParams(window.location.search);
     const productCategory = Number(urlParams.get('cat'));
-    const [product, setProduct] = useState<Product | null>(null);
     useEffect(() => {
         const fetchData = async () => {
-            const data = await fetchProduct();
-            setProducts(data);
 
-            const selectedProduct = products.find(product => product.category === productCategory);
-            setProduct(selectedProduct || null);
-            console.log('data = ', selectedProduct)
+            const productsData = await fetchProductCat(productCategory);
+            setProduct(productsData);
         };
 
         fetchData();
     }, []);
 
-    if (!product) {
-
-        return (
-            <div>
-                <main>
-                    <div className="produit-accueil me-3 p-lg-3 bg-light"><p>Chargement en cours...</p>;
-                    </div>
-                </main >
-            </div >
-        )
-    }
 
     return (
         <div>
             <main>
                 <div className="produit-accueil me-3 p-lg-3 bg-light">
+                    {products && products.length > 0 ? (
+                        products.map((product, index) => (
+                            <div key={index} className="container">
+                                <figure key={product.id} className="w-25 bg-light shadow-sm p-3 text-center">
+                                    <picture className="d-flex justify-center gap-5">
+                                        <i className="bi-heart"></i>
+                                    </picture>
+                                    <picture className="bg-body-secondary">
+                                        <p className="fw-semibold">{product.product_name}</p>
+                                        <img src={product.image} alt={'image' + ' ' + product.product_name} />
 
-                    <figure key={product.id} className="w-25 bg-light shadow-sm p-3 text-center">
-                        <picture className="d-flex justify-center gap-5">
-                            <i className="bi-heart"></i>
-                        </picture>
-                        <picture className="bg-body-secondary">
-                            <p className="fw-semibold">{product.product_name}</p>
-                            <img src={product.image} alt={'image' + ' ' + product.product_name} />
-
-                        </picture>
-                        <figcaption>
-                            <p> <strong>Prix {product.price}$</strong></p>
-                            <Link href={`/DetailProduit?id=${product.id}`}>
-                                <p>Voir le produits <i className="bi-arrow-right"></i></p>
-                            </Link>
-                        </figcaption>
-                    </figure>
+                                    </picture>
+                                    <figcaption>
+                                        <p> <strong>Prix {product.price}$</strong></p>
+                                        <Link href={`/DetailProduit?id=${product.id}`}>
+                                            <p>Voir le produits <i className="bi-arrow-right"></i></p>
+                                        </Link>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="produit-accueil me-3 p-lg-3 bg-light">
+                            <p>Aucun produit disponible.</p>
+                        </div>
+                    )}
                 </div>
             </main >
         </div >)
 }
+
+
 
 export default PoduitsTrie;
